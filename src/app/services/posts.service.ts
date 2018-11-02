61e5626ab09b1a8bfc505a0ccb026237b55f7c72
Jorge Dacosta <jorgemsdc@mail.com>
2018-11-02 22:23:53 +0100

    add posts functions

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
+  
+  // private API url
+  private url = 'https://jsonplaceholder.typicode.com/posts';
+  posts;
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  getPosts() {
+    this.http.get(this.url);
+  }
+
+  createPost(input: HTMLInputElement) {
+    let post = { title: input.value }
+    input.value = '';
+    this.http.post(this.url, post);
+  }
+
+  updatePost(post) {
+    this.http.patch(this.url + '/' + post.id , JSON.stringify({title: 'TITLE WAS UPDATED'}));
+  }
+
+  deletePost(post) {
+    this.http.delete(this.url + '/' + post.id);
+  }
+
 }
